b946d9d3170d5d8a98aa05d1ce101bdd288dc9c7
Tom Gregor <tgregor@ivsoftware.com>
2023-02-06 05:29:53 -0700

    Add project files.

---
+++ enabled-false-textbox-has-no-pointer/MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace enabled_false_textbox_has_no_pointer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new TextBoxEx();
+            this.checkBoxEnabled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(170, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Sample Text";
+            // 
+            // checkBoxEnabled
+            // 
+            this.checkBoxEnabled.AutoSize = true;
+            this.checkBoxEnabled.Checked = true;
+            this.checkBoxEnabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxEnabled.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBoxEnabled.Location = new System.Drawing.Point(35, 82);
+            this.checkBoxEnabled.Name = "checkBoxEnabled";
+            this.checkBoxEnabled.Size = new System.Drawing.Size(116, 34);
+            this.checkBoxEnabled.TabIndex = 1;
+            this.checkBoxEnabled.Text = "Enabled";
+            this.checkBoxEnabled.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.checkBoxEnabled);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBoxEx textBox1;
+        private System.Windows.Forms.CheckBox checkBoxEnabled;
+    }
+}
+
---
+++ enabled-false-textbox-has-no-pointer/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace enabled_false_textbox_has_no_pointer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            checkBoxEnabled.Checked = true;
+            checkBoxEnabled.CheckedChanged += (sender, e) =>
+            {
+                textBox1.Enabled = checkBoxEnabled.Checked;
+            };
+        }
+    }
+    class TextBoxEx : TextBox
+    {
+        const int WM_PAINT = 0x000F;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg.Equals(WM_PAINT) && !Enabled)
+            {
+                paintDisabled();
+                return;
+            }
+            base.WndProc(ref m);
+        }
+        private void paintDisabled()
+        {
+            using (Graphics graphics = CreateGraphics())
+            {
+                TextBoxRenderer.DrawTextBox(graphics, Bounds, System.Windows.Forms.VisualStyles.TextBoxState.Normal);
+            }
+        }
+    }
+}
